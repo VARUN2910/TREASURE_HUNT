db7b6ea891f8267e4a34ec0411b69fc1b5b56b7e
VARUN MAHESHWARI <78799620+VARUN2910@users.noreply.github.com>
2021-06-10 14:54:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+  bg = loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("treasure.jpg");
+}
+
+function setup() {
+  createCanvas(1000,500);
+  system = new System()
+  security = new Security()
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+
+  // Add code to display score in the middle of the screen
+  textSize(30);
+  fill("white");
+  text("SCORE : "+score,450,30)
+
+  // Add code to display the end screen
+
+
+  drawSprites()
+}
